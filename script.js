ce904af496bcfef8d141c323073b8015a0ac5d96
catherinestirk97 <53531093+catherinestirk97@users.noreply.github.com>
2020-09-29 10:54:51 +0100

    add files

---
+++ script.js
@@ -0,0 +1,31 @@
+const daysDisplay = document.getElementById("days")
+const hoursDisplay = document.getElementById('hours');
+const minutesDisplay = document.getElementById('minutes');
+const secondsDisplay = document.getElementById('seconds');
+
+const christmas = "25 Dec 2020"
+
+function countdown() {
+    const christmasDate = new Date(christmas);
+    const currentDate = new Date();
+
+    const totalSeconds = (christmasDate - currentDate) /1000;
+
+    const days = Math.floor(totalSeconds /3600/24);
+    const hours = Math.floor(totalSeconds / 3600) %24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysDisplay.innerHTML = days;
+    hoursDisplay.innerHTML = hours;
+    minutesDisplay.innerHTML = minutes;
+    secondsDisplay.innerHTML = seconds;
+
+    console.log(days, hours, minutes, seconds);
+}
+
+
+//initial call
+countdown();
+
+setInterval(countdown, 1000);
